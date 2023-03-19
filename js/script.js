70786b4c794a162203e8df07a3d82f44e3ca7ae7
Rob Schuster <robert.is@icloud.com>
2023-03-18 21:53:09 -0400

    JS constant Array set song list

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const epicMix = [
+  "Don't Steal My Sunshine",
+  "Walkin on the Sun",
+  "Black Velvanteen",
+  "Take a Chance on Me",
+  "Candy Perfume Girl",
+  "Call Me",
+  "Sunglasses at Night",
+  "Radar Love",
+  "I Wanna New Drug",
+  "I Can't Drive 55",
+  "Vogue",
+  "Don't Worry Be Happy",
+  "Poker Face"
+]
